036811716e4743c120a63b728376b0f2db615e7b
haashimalvi@protonmail.com <haashimalvi@protonmail.com>
2021-01-22 04:10:16 +0000

    modified the player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,28 @@
 extends KinematicBody2D
 
+#Velocity measures the change in x, & change in y position
+var velocity = Vector2.ZERO #A vector is the x and y position combined
 
 func _ready():
 	print("Hello World")
 
-#Every time the physics updates, this function is called	
+#This function is called every time the physics wants to update. Basically update() method	
 func _physics_process(delta):
-	if Input.is_action_pressed("ui")
+
+	var input_vector = Vector2.ZERO
+	"""
+	This isn't getting the exact coordinates. Action strength is only equal to 1 or 0. 
+	If you press one key, and not the opposite one, the action strength will equal to 1
+	This info is them passed to the move and collide method
+	"""
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
+	else:
+		velocity = Vector2.ZERO
+
+	
+	move_and_collide(velocity)	
 
